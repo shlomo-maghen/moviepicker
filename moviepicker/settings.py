36172e07c15c698bfc2f7b95547ddc4d01bc2de8
shlomo <shlomoshlomoshlomo@gmail.com>
2020-10-14 21:50:55 -0400

    Changed app name to rooms

--- moviepicker/settings.py
+++ moviepicker/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main.apps.MainConfig',
+    'rooms.apps.RoomsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
